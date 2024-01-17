584901f4f1cc2b9f2e4cb9684e8f7516af042f8b
RafaMadera18 <rafa_madlop@hotmail.com>
2024-01-17 11:42:45 -0600

    valiendo vrga

---
+++ src/Nodo.java
@@ -0,0 +1,21 @@
+public class Nodo {
+    private int valor;
+
+    private Nodo siguiente;
+
+    public int getValor() {
+        return this.valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Nodo getSiguiente() {
+        return this.siguiente;
+    }
+
+    public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }
+}
---
+++ src/Pila.java
@@ -0,0 +1,54 @@
+public class Pila {
+    private Nodo inicio;
+
+    private int tamano;
+
+    public boolean estaVacia() {
+        return this.inicio == null;
+    }
+
+    public int getTamano() {
+        return this.tamano;
+    }
+
+    public void apilar(int valor) {
+        var nuevo = new Nodo();
+        nuevo.setValor(valor);
+        if (estaVacia()) {
+            this.inicio = nuevo;
+        } else {
+            nuevo.setSiguiente(inicio);
+            inicio = nuevo;
+        }
+
+        this.tamano++;
+    }
+
+    public void retirar() {
+        if (!estaVacia()) {
+            inicio = inicio.getSiguiente();
+            tamano--;
+        }
+    }
+
+    public int cima() throws Exception {
+        if (estaVacia()) {
+            throw new Exception("La pila se encuentra vacia.");
+        }
+        return this.inicio.getValor();
+    }
+
+    public void eliminar(){
+        this.inicio = null;
+        this.tamano = 0;
+    }
+
+    public void listar(){
+        Nodo aux = this.inicio;
+        while (aux != null) {
+            System.out.println("|\t" + aux.getValor() + "\t|");
+            System.out.println("------------");
+            aux = aux.getSiguiente();
+        }
+    }
+}
